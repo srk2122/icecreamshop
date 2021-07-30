4e345808c67e091f0382b7bcafe42535e2fb5bc4
saurabh <saurabhkorgaonkar466@gmail.com>
2021-07-31 00:46:51 +0530

    Initial commit

---
+++ aboutus.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+ 
+    <div class="container " style="background-image: url(home3.jpg);  background-repeat: repeat;
+  background-size: 2000px;">
+        
+    <center><h1>About Us</h1></center>
+        <div class="card" style="background-image:url(college.jpg); width: 40rem; margin: 10% 30%;">
+            <div class="card-header">
+                <img class="card-img-top" src="hangyo.jpg">
+            </div>
+            <div class="card-body">
+              
+               <h5>
+               Hangyo Ice Creams Pvt. Ltd., is a flagship company of Srikrishna Group, which is a Rs.100 crore entity with interests spanning various sectors like milk and milk products, ice creams and fresh foods, restaurants and hospitality. Srikrishna Group has the distinction of being the first organized private sector venture in milk industry in Karnataka state. Continuing in the same pioneering tradition, Hangyo Ice Cream Pvt. Ltd., is also Karnataka’s first private ice cream manufacturer in the organized sector. Incorporated in 2002, Hangyo, is today a leading ice cream brand loved by consumers across all segments of society in its rapidly growing market spanning the states of Karnataka, Kerala and Goa.
+
+
+Hangyo Ice Cream Pvt. Ltd., is a ISO 22000:2005 certified company for both its divisions - Dairy and Ice Cream. Hangyo is probably the first ISO certified company in private sector in ice cream manufacture in the state.
+               </h5>
+                
+
+           
+        </div>
+   </div>
+
+  
+</img>
+
+
+</body>
+</html>
